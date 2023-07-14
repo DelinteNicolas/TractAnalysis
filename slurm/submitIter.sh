991506c965601c6800df7febc6c3f9a5747aef31
Nicolas Delinte <nicolas.delinte@uclouvain.be>
2023-07-14 14:28:54 +0200

    Added slum job for pval and corresponding tract extraction

--- slurm/submitIter.sh
+++ slurm/submitIter.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=$1
 #
 #SBATCH --ntasks=1
-#SBATCH --time=6:00:00
-#SBATCH --mem-per-cpu=8000
+#SBATCH --time=3:00:00
+#SBATCH --mem-per-cpu=10000
 #SBATCH --mail-type='FAIL'
 #SBATCH --mail-user='nicolas.delinte@uclouvain.be'
 
 #SBATCH --output='/home/users/n/d/ndelinte/Python/slurmJob.out'
 #SBATCH --error='/home/users/n/d/ndelinte/Python/slurmJob.err'
         
-python $2 $1 $3
+python $1 $2 $3
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from utils import print_views_from_study_folder
-from core import slurm_iter
-from core import significance_level
+from core import slurm_iter, significance_level, get_edges_of_interest
 
 
 if __name__ == '__main__':
@@ -11,12 +10,18 @@ if __name__ == '__main__':
         root.split('/')[-2] + '/', '') + 'TractAnalysis/'
 
     # Checking views
-    print_views_from_study_folder(root)
+    # print_views_from_study_folder(root)
 
-    slurm_iter(root, patient_list=['sub01_E1'])
+    # slurm_iter(root, 'connectivity', patient_list=['sub01_E1'])
 
     # Pvals
     subjects_list = root + 'subjects/subj_list.json'
     output_path = path_to_analysis_code + 'output_analysis/'
 
-    significance_level(subjects_list, root, output_path)
+    # significance_level(subjects_list, root, output_path)
+
+    pval_file = path_to_analysis_code + 'output_analysis/_pvals_E12_E13_E23.npy'
+
+    edge = get_edges_of_interest(pval_file, output_path=output_path)
+
+    slurm_iter(root, 'extraction', patient_list=['sub01_E1'])
