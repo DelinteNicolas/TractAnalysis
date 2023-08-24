5869074c59e96455e03712b7595c7bc1f38228d8
mdausort <manon.dausort@uclouvain.be>
2023-08-24 14:29:16 +0200

    graphs analysis

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import json
 from utils import (print_views_from_study_folder, get_mean_connectivity,
-                   check_labels, labels_matching)
+                   check_labels, labels_matching, metrics_analysis)
 from core import (slurm_iter, significance_level, get_edges_of_interest,
                   register_labels_to_atlas, get_mean_tracts_study)
 
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     mni_fa_path = path_to_analysis_code + 'data/FSL_HCP1065_FA_1mm.nii.gz'
     label_atlas_path = output_analysis_path + 'atlas_desikan_killiany_mni.nii.gz'
     freeSurfer_labels = path_to_analysis_code + 'data/FreeSurfer_labels.xlsx'
-    selected_edges_path = output_analysis_path+'selected_edges.json'
+    selected_edges_path = output_analysis_path + 'selected_edges.json'
 
     with open(subjects_list, 'r') as read_file:
         list_subjects = json.load(read_file)
@@ -28,6 +28,41 @@ if __name__ == '__main__':
     connectivity_matrix_index_file = (root + 'subjects/' + 'sub01_E1'
                                       + '/dMRI/tractography/' + 'sub01_E1'
                                       + '_labels_connectivity_matrix_sift.txt')
+
+    patient_list = ["sub01_E1", "sub01_E2", "sub02_E1", "sub02_E2", "sub02_E3",
+                    "sub03_E2", "sub03_E3", "sub04_E1", "sub04_E2", "sub05_E1",
+                    "sub05_E2", "sub06_E2", "sub07_E2", "sub08_E1", "sub08_E2",
+                    "sub09_E1", "sub09_E2", "sub09_E3", "sub10_E1", "sub11_E1",
+                    "sub11_E2", "sub12_E1", "sub12_E2", "sub13_E1", "sub13_E2",
+                    "sub14_E1", "sub14_E2", "sub15_E1", "sub15_E2", "sub16_E1",
+                    "sub17_E1", "sub17_E2", "sub17_E3", "sub18_E1", "sub18_E2",
+                    "sub19_E1", "sub19_E2", "sub20_E1", "sub20_E2", "sub20_E3",
+                    "sub21_E1", "sub21_E2", "sub22_E1", "sub22_E2", "sub22_E3",
+                    "sub23_E1", "sub24_E1", "sub24_E2", "sub25_E2", "sub26_E1",
+                    "sub26_E2", "sub27_E1", "sub27_E2", "sub27_E3", "sub28_E1",
+                    "sub28_E2", "sub29_E1", "sub29_E2", "sub30_E1", "sub30_E2",
+                    "sub30_E3", "sub31_E1", "sub31_E2", "sub32_E1", "sub32_E2",
+                    "sub33_E1", "sub33_E2", "sub34_E1", "sub34_E2", "sub34_E3",
+                    "sub35_E1", "sub35_E2", "sub36_E1", "sub36_E2", "sub36_E3",
+                    "sub37_E1", "sub37_E2", "sub38_E1", "sub39_E1", "sub39_E2",
+                    "sub39_E3", "sub40_E1", "sub40_E2", "sub41_E1", "sub41_E2",
+                    "sub42_E1", "sub42_E2", "sub43_E1", "sub43_E2", "sub44_E1",
+                    "sub45_E1", "sub45_E2", "sub45_E3", "sub46_E1", "sub46_E2",
+                    "sub47_E1", "sub48_E1", "sub48_E2", "sub50_E1", "sub50_E2",
+                    "sub51_E1", "sub51_E2", "sub52_E1", "sub52_E2", "sub53_E1",
+                    "sub53_E2", "sub54_E1", "sub54_E2", "sub55_E1", "sub55_E2",
+                    "sub56_E1", "sub57_E1", "sub57_E2", "sub58_E1", "sub58_E2",
+                    "sub59_E1", "sub59_E2", "sub60_E1", "sub60_E2", "sub61_E1",
+                    "sub61_E2", "sub62_E1", "sub62_E2", "sub63_E1", "sub63_E2",
+                    "sub64_E1", "sub64_E2", "sub65_E1", "sub65_E2", "sub66_E1",
+                    "sub66_E2", "sub67_E2", "sub68_E1", "sub68_E2",
+                    "sub69_E1", "sub69_E2", "sub70_E1", "sub70_E2", "sub71_E1",
+                    "sub71_E2", "sub72_E1", "sub72_E2", "sub73_E1", "sub73_E2"]
+
+    metric_name = ['FA', 'AD', 'RD', 'MD', 'noddi_fintra', 'noddi_fextra', 'noddi_fiso', 'noddi_odi',
+                   'diamond_wFA', 'diamond_wMD', 'diamond_wRD', 'diamond_wAD',
+                   'diamond_fractions_csf', 'diamond_fractions_ftot', 'mf_frac_ftot',
+                   'mf_wfvf', 'mf_frac_csf', 'mf_fvf_tot']
 
 # =============================================================================
 # First section - Connectivity
@@ -65,6 +100,8 @@ if __name__ == '__main__':
     # edge = get_edges_of_interest(pval_file, output_path=output_analysis_path,
     #                              min_path=min_path)
 
+    metrics_analysis(patient_list, root, output_analysis_path, metric_name, selected_edges_path)
+
     # print('Launching jobs to extract tract of interest')
     # slurm_iter(root, 'extraction')  # , patient_list=['sub01_E1'])
 
@@ -72,5 +109,5 @@ if __name__ == '__main__':
 # Third section - Computing tract microstructure
 # =============================================================================
 
-    print('Estimating mean tract microscture metrics')
-    slurm_iter(root, 'estimation', patient_list=['Third_section'])
+    # print('Estimating mean tract microscture metrics')
+    # slurm_iter(root, 'estimation', patient_list=['Third_section'])
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,22 @@
 import os
 import json
 import string
-import pandas as pd
-import numpy as np
 import xlsxwriter
+import numpy as np
+import pandas as pd
 import nibabel as nib
-from unravel.utils import tract_to_ROI
+import seaborn as sns
 import matplotlib.pyplot as plt
+from unravel.utils import tract_to_ROI
+from statannotations.Annotator import Annotator
+
+
+def to_float64(val):
+    """
+    Used if *val* is an instance of numpy.float32.
+    """
+
+    return np.float64(val)
 
 
 def get_view_from_data(data_path: str):
@@ -187,13 +197,28 @@ def check_labels(list_subjects: str, root: str, output_path: str):
 
 def metrics_analysis(list_subjects: list, root: str, output_path: str, metric_name: list, edge_name: str):
 
+    with open(edge_name, 'r') as read_file:
+        edge_name = json.load(read_file)
+
+    print(edge_name)
+
     dataframe = {}
 
-    for i, r in enumerate(edge_name):
-        for j, sub in enumerate(list_subjects):
+    dataframe['Mean'] = {}
+    dataframe['Dev'] = {}
+
+    for j, sub in enumerate(list_subjects):
+
+        dataframe['Mean'][sub] = {}
+        dataframe['Dev'][sub] = {}
+
+        for i, r in enumerate(edge_name):
+
+            dataframe['Mean'][sub][str(r[0]) + '_' + str(r[1])] = {}
+            dataframe['Dev'][sub][str(r[0]) + '_' + str(r[1])] = {}
 
             ROI = tract_to_ROI(
-                root + '/subjects/' + sub + '/dMRI/tractography/tois/' + sub + '_tractogram_sift_' + r + '.trk')
+                root + '/subjects/' + sub + '/dMRI/tractography/tois/' + sub + '_tractogram_sift_' + str(r[0]) + '_' + str(r[1]) + '.trk')
 
             for k, m in enumerate(metric_name):
 
@@ -201,7 +226,7 @@ def metrics_analysis(list_subjects: list, root: str, output_path: str, metric_na
                     model = 'dti'
                 elif m in ['noddi_fintra', 'noddi_fextra', 'noddi_fiso', 'noddi_odi']:
                     model = 'noddi'
-                elif m in ['wFA', 'wMD', 'wRD', 'wAD', 'diamond_fractions_ftot', 'diamond_fractions_csf']:
+                elif m in ['diamond_wFA', 'diamond_wMD', 'diamond_wRD', 'diamond_wAD', 'diamond_fractions_ftot', 'diamond_fractions_csf']:
                     model = 'diamond'
                 else:
                     model = 'mf'
@@ -214,20 +239,16 @@ def metrics_analysis(list_subjects: list, root: str, output_path: str, metric_na
                 mean_ROI = np.mean(metric_in_ROI[metric_in_ROI != 0])
                 std_ROI = np.std(metric_in_ROI[metric_in_ROI != 0])
 
-                if(np.isnan(mean_ROI) == True):
+                if(np.isnan(mean_ROI)):
                     mean_ROI = 0
-                if(np.isnan(std_ROI) == True):
+                if(np.isnan(std_ROI)):
                     std_ROI = 0
 
-                dataframe['Mean', sub, r, m] = mean_ROI
-                dataframe['Dev', sub, r, m] = std_ROI
+                dataframe['Mean'][sub][str(r[0]) + '_' + str(r[1])][m] = mean_ROI
+                dataframe['Dev'][sub][str(r[0]) + '_' + str(r[1])][m] = std_ROI
 
-    dataframe2 = pd.DataFrame(dataframe, index=['Value']).T
-    dataframe2 = dataframe2.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
-    dataframe2 = dataframe2.sort_values(by='Dic', ascending=False)
-
-    return dataframe2
-
+    json.dump(dataframe, open(output_path + 'essai_metric_analysis.json', 'w'),
+              default=to_float64)
 
 def labels_matching(excel_path, connectivity_matrix_index_file):
 
@@ -244,10 +265,224 @@ def labels_matching(excel_path, connectivity_matrix_index_file):
     df.to_excel(excel_path.replace('.xlsx', '_bis.xlsx'))
 
 
-def data_to_json(excel_path: str):
+def jsonToPandas(jsonFilePath: str):
 
-    df = pd.read_excel(excel_path)
+    import pandas as pd
 
-    json_path = excel_path.replace('.xlsx', '.json')
+    file = open(jsonFilePath)
+    dic = json.load(file)
+    # dic = dic['Mean']
+    file.close()
 
-    df2 = df.to_json(json_path, orient='columns')
+    reform = {(level1_key, level2_key, level3_key, level4_key): values
+              for level1_key, level2_dict in dic.items()
+              for level2_key, level3_dict in level2_dict.items()
+              for level3_key, level4_dict in level3_dict.items()
+              for level4_key, values in level4_dict.items()}
+
+    p = pd.DataFrame(reform, index=['Value']).T
+    p = p.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
+
+    return p
+
+
+def dictionary_without_controls(path_json, control_list):
+
+    data = jsonToPandas(path_json)
+    data_value = np.array(data)
+    new_data_value = data_value
+
+    indexes = list(data.index)
+    for i, j in enumerate(indexes):
+        indexes[i] = np.array(j)
+    indexes = np.array(indexes)
+
+    new_indexes = indexes
+    list_subj = new_indexes[:, 1]
+
+    for i in range(len(list_subj) - 1, -1, -1):
+        if list_subj[i] in control_list:
+            new_data_value = np.delete(new_data_value, i, axis=0)
+            new_indexes = np.delete(new_indexes, i, axis=0)
+
+    dataframe = {}
+    for i in range(len(new_data_value)):
+        dataframe[str(new_indexes[i, 0]), str(new_indexes[i, 1]), str(new_indexes[i, 2]), str(new_indexes[i, 3])] = new_data_value[i][0]
+    dataframe2 = pd.DataFrame(dataframe, index=['Value']).T
+    dataframe2 = dataframe2.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
+    dataframe2 = dataframe2.sort_values(by='Dic', ascending=False)
+
+    return dataframe2
+
+
+def dictionary_controls(path_json, control_list):
+
+    data = jsonToPandas(path_json)
+    data_value = np.array(data)
+    new_data_value = data_value
+
+    indexes = list(data.index)
+    for i, j in enumerate(indexes):
+        indexes[i] = np.array(j)
+    indexes = np.array(indexes)
+
+    new_indexes = indexes
+    list_subj = new_indexes[:, 1]
+
+    for i in range(len(list_subj) - 1, -1, -1):
+        if list_subj[i] not in control_list:
+            new_data_value = np.delete(new_data_value, i, axis=0)
+            new_indexes = np.delete(new_indexes, i, axis=0)
+
+    dataframe = {}
+
+    for i in range(len(new_data_value)):
+        dataframe[str(new_indexes[i, 0]), str(new_indexes[i, 1]), str(new_indexes[i, 2]), str(new_indexes[i, 3])] = new_data_value[i][0]
+
+    dataframe2 = pd.DataFrame(dataframe, index=['Value']).T
+    dataframe2 = dataframe2.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
+    dataframe2 = dataframe2.sort_values(by='Dic', ascending=False)
+
+    return dataframe2
+
+
+def list_nom_patient(dataframe, control_list):
+
+    patients = dataframe.index.unique(1)
+    name_controls = []
+    name_patients = []
+
+    for i in patients:
+        if i in control_list:
+            i = i.replace('_E1', '')
+            i = i.replace('_E2', '')
+            i = i.replace('_E3', '')
+
+            if i not in name_controls:
+                name_controls.append(i)
+        else:
+            i = i.replace('_E1', '')
+            i = i.replace('_E2', '')
+            i = i.replace('_E3', '')
+
+            if i not in name_patients:
+                name_patients.append(i)
+
+    return name_patients, name_controls
+
+
+def difference_temps(dataframe, control_list, dic_val):
+
+    name_patients, name_controls = list_nom_patient(dataframe, control_list)
+    name_all = np.append(name_patients, name_controls)
+
+    df_temps = {}
+    list_temps = []
+
+    for t in list(dataframe.index.unique(1)):
+        if t[-2:] not in list_temps:
+            list_temps.append(t[-2:])
+
+    list_temps.sort()
+
+    for region_val in list(dataframe.index.unique(2)):
+        for metric_val in list(dataframe.index.unique(3)):
+            for i in name_all:
+                inter_value = []
+                for temps in list_temps:
+                    try:
+                        inter_value.append(dataframe.loc[dic_val, i + '_' + temps, region_val, metric_val][0])
+                    except KeyError:
+                        inter_value.append(float('nan'))
+                        continue
+
+                df_temps[dic_val, i, region_val, metric_val] = inter_value
+
+    temps_dataframe = pd.DataFrame(df_temps, index=[list_temps]).T
+    temps_dataframe = temps_dataframe.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
+    temps_dataframe = temps_dataframe.sort_values(by='Patient', ascending=True)
+
+    temps_dataframe['Diff E2-E1'] = np.array(temps_dataframe.loc[:, 'E2']) - np.array(temps_dataframe.loc[:, 'E1'])
+    temps_dataframe['Diff E3-E1'] = np.array(temps_dataframe.loc[:, 'E3']) - np.array(temps_dataframe.loc[:, 'E1'])
+    temps_dataframe['Diff E3-E2'] = np.array(temps_dataframe.loc[:, 'E3']) - np.array(temps_dataframe.loc[:, 'E2'])
+
+    return temps_dataframe
+
+
+def graphs_analysis(path_json, specific, region, control_list, dic=None, metric=None):
+
+    data = jsonToPandas(path_json)
+    data_value = np.array(data)
+    new_data_value = data_value
+
+    indexes = list(data.index)
+    for i, j in enumerate(indexes):
+        indexes[i] = np.array(j)
+    indexes = np.array(indexes)
+
+    new_indexes = indexes
+    list_subj = new_indexes[:, 1]
+
+    for i in range(len(list_subj) - 1, -1, -1):
+        if list_subj[i] in control_list:
+            new_data_value = np.delete(new_data_value, i, axis=0)
+            new_indexes = np.delete(new_indexes, i, axis=0)
+
+    dataframe = {}
+    for i in range(len(new_data_value)):
+        dataframe[str(new_indexes[i, 0]), str(new_indexes[i, 1]), str(new_indexes[i, 2]), str(new_indexes[i, 3])] = new_data_value[i][0]
+    dataframe2 = pd.DataFrame(dataframe, index=['Value']).T
+    dataframe2 = dataframe2.rename_axis(['Dic', 'Patient', 'Region', 'Metric'])
+    dataframe2 = dataframe2.sort_values(by='Dic', ascending=False)
+
+    temps_list = list(dataframe2.index.unique(1))
+    for i, j in enumerate(temps_list):
+        if "E1" in j and j not in control_list:
+            temps_list[i] = "E1"
+        elif "E2" in j and j not in control_list:
+            temps_list[i] = "E2"
+        elif "E3" in j and j not in control_list:
+            temps_list[i] = "E3"
+
+    if specific:
+        assert dic != None, 'To compute the graphs, the dic variable must have a value set to something other than None'
+        assert metric != None, 'To compute the graphs, the metric variable must have a value set to something other than None'
+
+        df = pd.DataFrame({'Temps': temps_list, 'Value': np.squeeze(dataframe2.loc[dic, :, region, metric])})
+
+        fig = plt.figure()
+        ax = sns.violinplot(x='Temps', y='Value', data=df, order=['E1', 'E2', 'E3'])
+        sns.stripplot(x="Temps", y="Value", data=df, jitter=True, order=['E1', 'E2', 'E3'], color='w')
+        plt.ylabel(metric)
+        plt.title(metric + ' values of all patient for the edge ' + region)
+
+        pairs = [('E1', 'E2'), ('E1', 'E3'), ('E3', 'E2')]
+        annotator = Annotator(ax, pairs, data=df, x='Temps', y='Value', order=['E1', 'E2', 'E3'])
+        annotator.configure(test="t-test_ind", text_format='simple', loc='inside')
+        a = annotator.apply_and_annotate()
+        plt.tight_layout()
+        plt.show()
+
+    else:
+        pval = {}
+
+        for dic_val in list(dataframe2.index.unique(0)):
+            for region_val in list(dataframe2.index.unique(2)):
+                for metric_val in list(dataframe2.index.unique(3)):
+                    df = pd.DataFrame({'Temps': temps_list, 'Value': np.squeeze(dataframe2.loc[dic_val, :, region_val, metric_val])})
+
+                    fig = plt.figure()
+                    ax = sns.violinplot(x='Temps', y='Value', data=df, order=['E1', 'E2', 'E3'])
+                    plt.ylabel(metric)
+                    plt.title(dic_val + ' ' + metric_val + ' values of all patient for the edge ' + region_val)
+
+                    pairs = [('E1', 'E2'), ('E2', 'E3'), ('E1', 'E3')]
+                    annotator = Annotator(ax, pairs, data=df, x='Temps', y='Value', order=['E1', 'E2', 'E3'])
+                    annotator.configure(test="t-test_welch", text_format='simple', loc='inside')
+                    a = annotator.apply_and_annotate()
+
+                    pval[dic_val, region_val, metric_val] = np.array([a[1][0].data.pvalue, a[1][1].data.pvalue, a[1][2].data.pvalue])
+
+        pval_dataframe = pd.DataFrame(pval, index=['E1-E2', 'E2-E3', 'E1-E3']).T
+        pval_dataframe = pval_dataframe.rename_axis(['Dic', 'Region', 'Metric'])
+        pval_dataframe = pval_dataframe.sort_values(by='Dic', ascending=False)
